4af94859f8f69382e3792be559899c0e3f8afe21
Smile Kun <smiling.cornflower@gmail.com>
2026-01-17 22:45:07 +0500

    fix: escaping percent(%) sign in database url

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -28,7 +28,8 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-config.set_main_option("sqlalchemy.url", str(settings.db.url))
+url = str(settings.db.url).replace("%", "%%")
+config.set_main_option("sqlalchemy.url", url)
 
 
 def run_migrations_offline() -> None:
