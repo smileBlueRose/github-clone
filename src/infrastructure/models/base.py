f509eed9b1ebf9f0e5dafb9aa9af4aaf41d67163
Smile Kun <smiling.cornflower@gmail.com>
2026-01-17 23:26:56 +0500

    feat: add naming convention for models

--- src/infrastructure/models/base.py
+++ src/infrastructure/models/base.py
@@ -1,5 +1,10 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
+
+from config import settings
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
