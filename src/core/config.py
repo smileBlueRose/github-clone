4d3b12981d59572f691eb4d11e933f9005291897
Smile Kun <smiling.cornflower@gmail.com>
2026-01-17 05:15:45 +0500

    feat: add DatabaseHelper

--- src/core/config.py
+++ src/core/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings
 
 
@@ -7,11 +7,23 @@ class RunConfig(BaseModel):
     port: int = 5000
     reload: bool = True
 
+
 class ApiConfig(BaseModel):
-    prefix: str = '/api'
+    prefix: str = "/api"
+
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
 
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiConfig = ApiConfig()
+    db: DatabaseConfig
+
 
 settings = Settings()
--- src/main.py
+++ src/main.py
@@ -8,9 +8,11 @@ from core.config import settings
 app = Flask(__name__)
 app.register_blueprint(api_router, url_prefix=settings.api.prefix)
 
+
 @app.get("/")
 async def index() -> dict[str, str]:
     return {"message": "Hello World!"}
+
 
 asgi_app = WsgiToAsgi(app)  # type: ignore
 
@@ -19,5 +21,5 @@ if __name__ == "__main__":
         "main:asgi_app",
         host=settings.run.host,
         port=settings.run.port,
-        reload=settings.run.reload
-        )
+        reload=settings.run.reload,
+    )
