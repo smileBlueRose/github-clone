dbba5d439674ae71dc61d6fd7049d188434234f2
Smile Kun <smiling.cornflower@gmail.com>
2026-01-18 09:02:21 +0500

    feat: update TokenService, add methods for refresh token: generate and verify.

--- src/domain/services/auth.py
+++ src/domain/services/auth.py
@@ -1,4 +1,5 @@
 from datetime import UTC, datetime
+from uuid import uuid4
 
 import jwt
 from loguru import logger
@@ -7,7 +8,12 @@ from config import settings
 from domain.entities.user import User
 from domain.exceptions.auth import InvalidTokenException, TokenExpiredException
 from domain.ports.service import BaseService
-from domain.value_objects.token import AccessToken, AccessTokenPayload
+from domain.value_objects.token import (
+    AccessToken,
+    AccessTokenPayload,
+    RefreshToken,
+    RefreshTokenPayload,
+)
 
 
 class TokenService(BaseService):
@@ -23,6 +29,9 @@ class TokenService(BaseService):
         self.access_token_lifetime = access_token_lifetime
         self.refresh_token_lifetime = refresh_token_lifetime
 
+    # ================
+    # ==== ACCESS ====
+    # ================
     def generate_access(self, user: User) -> AccessToken:
         """Generate a new access token for a user."""
         issued_at = int(datetime.now(UTC).timestamp())
@@ -80,3 +89,63 @@ class TokenService(BaseService):
         except (jwt.PyJWTError, ValueError) as e:
             logger.exception(e)
             raise InvalidTokenException("Invalid access token.") from e
+
+    # =================
+    # ==== REFRESH ====
+    # =================
+    def generate_refresh(self, user: User) -> RefreshToken:
+        """Generate a new refresh token for a user."""
+        issued_at = int(datetime.now(UTC).timestamp())
+        expires_at = issued_at + self.refresh_token_lifetime
+
+        payload = RefreshTokenPayload(
+            sub=user.id,
+            iat=issued_at,
+            exp=expires_at,
+            jti=uuid4(),
+        )
+        token: str = jwt.encode(
+            payload.model_dump(mode="json"),
+            key=self.__private_key,
+            algorithm=settings.auth.jwt.algorithm,
+        )
+        return RefreshToken(value=token)
+
+    def _verify_refresh(self, token: RefreshToken) -> RefreshTokenPayload:
+        """
+        Verify refresh token and return its payload.
+
+        :raises ExpiredSignatureError: If the token has expired.
+        :raises PyJWTError: If the token is invalid for any other reason
+                            (e.g., malformed, invalid signature, or missing required claims).
+        :raises ValueError: If payload['type'] is not TokenTypeEnum.REFRESH
+        """
+        payload = jwt.decode(
+            token.value,
+            key=self.public_key,
+            algorithms=[settings.auth.jwt.algorithm],
+            options=settings.auth.jwt.refresh_decode_options,
+        )
+
+        return RefreshTokenPayload(
+            sub=payload["sub"],
+            iat=payload["iat"],
+            exp=payload["exp"],
+            jti=payload["jti"],
+            type=payload["type"],
+        )
+
+    def verify_refresh(self, token: RefreshToken) -> RefreshTokenPayload:
+        """
+        Verify refresh token and return its payload.
+        :raises TokenExpiredException:
+        :raises InvalidTokenException: If token verification fails.
+        """
+        try:
+            return self._verify_refresh(token)
+        except jwt.ExpiredSignatureError as e:
+            logger.exception(e)
+            raise TokenExpiredException("Refresh token has expired.") from e
+        except (jwt.PyJWTError, ValueError) as e:
+            logger.exception(e)
+            raise InvalidTokenException("Invalid refresh token.") from e
