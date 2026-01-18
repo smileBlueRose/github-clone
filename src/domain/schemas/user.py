11dc1a26f10be8f0f8e083e47fe522ef9cca87d6
Smile Kun <smiling.cornflower@gmail.com>
2026-01-18 18:13:47 +0500

    feat: update UserCreateSchema

--- src/domain/schemas/user.py
+++ src/domain/schemas/user.py
@@ -1,8 +1,12 @@
+from pydantic import EmailStr
+
 from domain.ports.schemas import BaseCreateSchema, BaseUpdateSchema
 
 
 class UserCreateSchema(BaseCreateSchema):
-    pass
+    email: EmailStr
+    username: str
+    hashed_password: str
 
 
 class UserUpdateSchema(BaseUpdateSchema):
