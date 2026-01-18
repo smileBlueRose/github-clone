09c5f2a94ea8edfa1e83bbd18be0c56052442f7c
Smile Kun <smiling.cornflower@gmail.com>
2026-01-18 09:11:29 +0500

    tests: add tests for refresh generate and verify

--- tests/unit/auth/test_auth.py
+++ tests/unit/auth/test_auth.py
@@ -1,4 +1,5 @@
 from unittest.mock import Mock
+from uuid import UUID
 
 import jwt
 import pytest
@@ -8,7 +9,7 @@ from domain.entities.user import User
 from domain.enums.token_type import TokenTypeEnum
 from domain.exceptions.auth import InvalidTokenException, TokenExpiredException
 from domain.services.auth import TokenService
-from domain.value_objects.token import AccessToken, AccessTokenPayload
+from domain.value_objects.token import AccessToken, AccessTokenPayload, RefreshToken
 
 LIFETIME = 3600
 
@@ -21,6 +22,9 @@ def token_service(private_key: str, public_key: str) -> TokenService:
     )
 
 
+# ================
+# ==== ACCESS ====
+# ================
 @pytest.fixture(scope="function")
 def token_service_with_another_keys(another_private_key: str, another_public_key: str) -> TokenService:
     return TokenService(
@@ -80,3 +84,71 @@ def test_verify_access_without_required_field(token_service: TokenService, user:
         )
         with pytest.raises(InvalidTokenException):
             token_service.verify_access(AccessToken(value=bad_token_value))
+
+
+# =================
+# ==== REFRESH ====
+# =================
+def test_generate_and_verify_refresh_success(token_service: TokenService, user: User) -> None:
+    token = token_service.generate_refresh(user)
+    payload = token_service.verify_refresh(token)
+
+    assert payload.sub == user.id
+    assert payload.type == TokenTypeEnum.REFRESH
+    assert isinstance(payload.jti, UUID)
+
+
+def test_verify_refresh_expired(token_service: TokenService, user: User) -> None:
+    token_service.refresh_token_lifetime = 0
+    token = token_service.generate_refresh(user)
+
+    with pytest.raises(TokenExpiredException):
+        token_service.verify_refresh(token)
+
+
+def test_verify_refresh_with_incorrect_token(token_service: TokenService, user: User) -> None:
+    token = token_service.generate_refresh(user)
+    invalid_token = RefreshToken(value=token.value + "garbage")
+
+    with pytest.raises(InvalidTokenException):
+        token_service.verify_refresh(invalid_token)
+
+
+def test_verify_refresh_another_private_key(
+    token_service: TokenService,
+    token_service_with_another_keys: TokenService,
+    user: User,
+) -> None:
+    token = token_service_with_another_keys.generate_refresh(user)
+
+    with pytest.raises(InvalidTokenException):
+        token_service.verify_refresh(token)
+
+
+def test_verify_refresh_without_required_field(token_service: TokenService, user: User, private_key: str) -> None:
+    payload = token_service.verify_refresh(token_service.generate_refresh(user))
+
+    for field in settings.auth.jwt.refresh_decode_options["require"]:
+        bad_payload = payload.model_dump(mode="json")
+        bad_payload.pop(field)
+
+        bad_token_value = jwt.encode(
+            bad_payload,
+            key=private_key,
+            algorithm=settings.auth.jwt.algorithm,
+        )
+        with pytest.raises(InvalidTokenException):
+            token_service.verify_refresh(RefreshToken(value=bad_token_value))
+
+
+def test_cross_token_verification_fail(token_service: TokenService, user: User) -> None:
+    access_token = token_service.generate_access(user)
+    refresh_token = token_service.generate_refresh(user)
+
+    # Access should not pass verification as Refresh
+    with pytest.raises(InvalidTokenException):
+        token_service.verify_refresh(RefreshToken(value=access_token.value))
+
+    # Refresh should not pass verification as Access
+    with pytest.raises(InvalidTokenException):
+        token_service.verify_access(AccessToken(value=refresh_token.value))
