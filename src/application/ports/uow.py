4f330646eb63e3d0f3f3cab5d6c47e908673ec56
Smile Kun <smiling.cornflower@gmail.com>
2026-01-19 08:45:29 +0500

    feat: add SqlAlchemyUoW

---
+++ src/application/ports/uow.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from types import TracebackType
+from typing import Any, Self
+
+
+class AbstractUnitOfWork(ABC):
+    session: Any
+
+    @abstractmethod
+    async def __aenter__(self) -> Self:
+        pass
+
+    @abstractmethod
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException,
+        exc_tb: TracebackType,
+    ) -> None:
+        pass
+
+    @abstractmethod
+    async def commit(self) -> None:
+        pass
+
+    @abstractmethod
+    async def rollback(self) -> None:
+        pass
---
+++ src/infrastructure/uow/sqlalchemy.py
@@ -0,0 +1,29 @@
+from types import TracebackType
+from typing import Callable, Self, cast
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from application.ports.uow import AbstractUnitOfWork
+
+
+class SqlAlchemyUoW(AbstractUnitOfWork):
+    def __init__(self, session_factory: Callable[[], AsyncSession]) -> None:
+        self.session_factory = session_factory
+        self.session: AsyncSession | None = None
+
+    async def __aenter__(self) -> Self:
+        self.session = self.session_factory()
+        return self
+
+    async def __aexit__(
+        self, exc_type: type[BaseException] | None, exc_val: BaseException, exc_tb: TracebackType
+    ) -> None:
+        if exc_type:
+            await self.rollback()
+        await cast(AsyncSession, self.session).close()
+
+    async def commit(self) -> None:
+        await cast(AsyncSession, self.session).commit()
+
+    async def rollback(self) -> None:
+        await cast(AsyncSession, self.session).rollback()
