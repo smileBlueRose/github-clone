a40e1d2817281494ce228be3d708e0f91f8050bf
Smile Kun <smiling.cornflower@gmail.com>
2026-01-21 16:46:42 +0500

    fix: rename  hashed_password to password_hash

--- tests/utils.py
+++ tests/utils.py
@@ -9,12 +9,12 @@ from infrastructure.database.models.user import UserModel
 async def create_user_model(session: AsyncSession, **kwargs: Any) -> UserModel:
     if "password" in kwargs:
         password = kwargs.pop("password")
-        kwargs["hashed_password"] = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        kwargs["password_hash"] = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
 
     defaults = {
         "email": "test@example.com",
         "username": "ScarletScarf",
-        "hashed_password": "$2b$12$6rEUV1t6EHbC.HnelrKE9OntR1MUITbOJqteguORQ0un0CIRu/EzS",
+        "password_hash": "$2b$12$6rEUV1t6EHbC.HnelrKE9OntR1MUITbOJqteguORQ0un0CIRu/EzS",
     }
     defaults.update(kwargs)
     user = UserModel(**defaults)
