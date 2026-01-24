c51c97da70e6158da90ab3681382db7316c489fd
Scarlet Scarf <156634569+smileBlueRose@users.noreply.github.com>
2026-01-24 12:58:36 +0500

    feat: add GitRepoWriteRepository & GitRepoReadRepository

---
+++ src/infrastructure/repositories/git_repo.py
@@ -0,0 +1,73 @@
+from uuid import UUID
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from domain.entities.git import Repository
+from domain.exceptions.git import GitRepositoryNotFoundException
+from domain.filters.git import GitRepoFilter
+from domain.ports.repositories.git_repo import (
+    AbstractGitRepoReadRepository,
+    AbstractGitRepoWriteRepository,
+)
+from domain.schemas.repository_storage import GitRepoCreateSchema, GitRepoUpdateSchema
+from infrastructure.database.models.repository import RepositoryModel
+
+
+class GitRepoWriteRepository(AbstractGitRepoWriteRepository):
+    def __init__(self, session: AsyncSession) -> None:
+        self._session = session
+
+    async def create(self, schema: GitRepoCreateSchema) -> Repository:
+        repo_model = RepositoryModel(
+            name=schema.name,
+            owner_id=schema.owner_id,
+            description=schema.description,
+        )
+        self._session.add(repo_model)
+        await self._session.flush()
+        return repo_model.to_entity()
+
+    async def update(self, identity: UUID, schema: GitRepoUpdateSchema) -> Repository:
+        repo_model = await self._session.get(RepositoryModel, identity)
+        if repo_model is None:
+            raise GitRepositoryNotFoundException(repo_id=identity)
+
+        if schema.name is not None:
+            repo_model.name = schema.name
+        if schema.description is not None:
+            repo_model.description = schema.description
+
+        await self._session.flush()
+        return repo_model.to_entity()
+
+    async def delete_by_identity(self, identity: UUID) -> bool:
+        repo_model = await self._session.get(RepositoryModel, identity)
+
+        if repo_model is None:
+            return False
+
+        await self._session.delete(repo_model)
+        await self._session.flush()
+        return True
+
+
+class GitRepoReadRepository(AbstractGitRepoReadRepository):
+    def __init__(self, session: AsyncSession) -> None:
+        self._session = session
+
+    async def get_by_identity(self, identity: UUID) -> Repository:
+        """:raises GitRepositoryNotFoundException:"""
+
+        repo_model = await self._session.get(RepositoryModel, identity)
+
+        if repo_model is None:
+            raise GitRepositoryNotFoundException(repo_id=identity)
+
+        return repo_model.to_entity()
+
+    async def get_all(self, filter_: GitRepoFilter) -> list[Repository]:
+        stmt = select(RepositoryModel)
+
+        result = await self._session.execute(stmt)
+        return [i.to_entity() for i in result.scalars().all()]
