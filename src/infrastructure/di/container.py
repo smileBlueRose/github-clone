ecfab236c451a6e8ee13a1ccbc80c6c8c75f5dfe
Scarlet Scarf <156634569+smileBlueRose@users.noreply.github.com>
2026-01-25 18:58:10 +0500

    feat: add storages container

--- src/infrastructure/di/container.py
+++ src/infrastructure/di/container.py
@@ -2,15 +2,18 @@ from dependency_injector import containers, providers
 
 from infrastructure.di.containers.database import DatabaseContainer
 from infrastructure.di.containers.services import ServiceContainer
+from infrastructure.di.containers.storages import StorageContainer
 from infrastructure.di.containers.use_cases import UseCaseContainer
 
 
 class Container(containers.DeclarativeContainer):
     database = providers.Container(DatabaseContainer)
     services = providers.Container(ServiceContainer)
+    storages = providers.Container(StorageContainer)
 
     use_cases = providers.Container(
         UseCaseContainer,
         database=database,
         services=services,
+        storages=storages,
     )
