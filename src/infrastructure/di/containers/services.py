370447fff6569e53a0aa881ff87c0ae4bcae37ea
Scarlet Scarf <156634569+smileBlueRose@users.noreply.github.com>
2026-01-26 02:21:50 +0500

    feat: add policy service to container

--- src/infrastructure/di/containers/services.py
+++ src/infrastructure/di/containers/services.py
@@ -1,7 +1,9 @@
 from dependency_injector import containers, providers
 
 from config import settings
+from config.config import BASE_DIR
 from domain.services.auth.token import TokenService
+from infrastructure.policy_loader import PolicyLoader
 
 
 class ServiceContainer(containers.DeclarativeContainer):
@@ -10,3 +12,4 @@ class ServiceContainer(containers.DeclarativeContainer):
         private_key=settings.auth.jwt.private_key,
         public_key=settings.auth.jwt.public_key,
     )
+    policy_service = providers.Singleton(PolicyLoader.load_from_yaml, file_path=BASE_DIR / settings.policies_file_path)
